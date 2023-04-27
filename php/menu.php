7db2269d3d7db3b78ef35f704331ebe0c494627d
mtereso <marco.tereso@islasantarem.pt>
2023-04-27 19:51:32 +0100

    versao modificada menu

--- php/menu.php
+++ php/menu.php
@@ -1,11 +1,16 @@
 <?php
+
+$path_home = '#';
+$path_regiso = 'php/registo.php';
+$path_contatos = 'php/contatos.php';
+
 $menu = <<<MENU
 
 <nav id="menu1">
 	<ul>
-		<li><a href="#">Home</a></li>
-		<li><a href="php/registo.php">Registo</a></li>
-		<li><a href="php/registos.php">Contatos</a></li>
+		<li><a href="$path_home">Home</a></li>
+		<li><a href="$path_regiso">Registo</a></li>
+		<li><a href="$path_contatos">Contatos</a></li>
 	</ul>
 </nav>
 MENU;
--- php/valida.php
+++ php/valida.php
@@ -1,11 +1,19 @@
+<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="../css/estilo.css">
+
 <?php
-
+//declarar variáveis e associar ao conteúdo do formulário
 $nome = $_POST['nome'];
-
+$login = $_POST['login'];
+$morada = $_POST['morada'];
+$instagram = $_POST['insta'];
+$email = $_POST['email'];
+$nif = $_POST['nif'];
+$telef = $_POST['telef'];
 $pass = $_POST['pass'];
 $repass = $_POST['repass'];   
 
-
+//validação dos campos de formulário
 if(is_null($pass) || is_null($repass)){
 	echo "<script>
 				alert('Preencha os campos password'); window.location.href = 'registo.php'; 
@@ -17,3 +25,28 @@ if($pass != $repass){
 }
 
 ?>
+
+<table id="tabela_dados" class="table table-dark table-striped">
+  <thead>
+  	<th>Nome</th>
+  	<th>Login</th>
+  	<th>Morada</th>
+  	<th>Instagram</th>
+  	<th>Email</th>
+  	<th>NIF</th>
+  	<th>Telefone</th>
+  	<th>Password</th>
+  </thead>
+  <tbody>
+  	<tr>
+  		<td><?php echo $nome; ?></td>
+  		<td><?php echo $login; ?></td>
+  		<td><?php echo $morada; ?></td>
+  		<td><?php echo $instagram; ?></td>
+  		<td><?php echo $email; ?></td>
+  		<td><?php echo $nif; ?></td>
+  		<td><?php echo $telef; ?></td>
+  		<td><?php echo $pass; ?></td>
+  	</tr>
+  </tbody>
+</table>
